ab9b3117830c33dde79bfd90aaa63f17b0d9f112
Chester Ismay <chester.ismay@gmail.com>
2023-11-20 15:08:23 -1000

    Update build

---
+++ docs/outlines/mind_maps.R
@@ -0,0 +1,26 @@
+# install.packages("remotes")
+# remotes::install_github("pzhaonet/mindr")
+library(mindr)
+
+create_mindmaps <- function(input_file) {
+
+  input <- input_file
+  input_txt <- readLines(input, encoding = "UTF-8")
+
+  ## Convert to mind map text, markdown outline, and HTML widget
+  mm_output <- mm(input_txt, output_type = c("mindmap", "markdown", "widget"))
+  mm_output
+
+}
+
+v2_outline_widget <- create_mindmaps(input_file = "07-sampling.Rmd")$widget
+v1_outline_widget <- create_mindmaps(input_file = "07-sampling-OLD.Rmd")$widget
+
+# Combined mindmap copying all of the 07-sampling-OLD.Rmd text and then the
+# 07-sampling.Rmd text into the ch7-v1-v2.Rmd file
+v1_v2_outline_widget <- create_mindmaps(input_file = "outlines/ch7-v1-v2.Rmd")$widget
+
+# Open widgets in RStudio Viewer
+v1_outline_widget
+v2_outline_widget
+v1_v2_outline_widget
